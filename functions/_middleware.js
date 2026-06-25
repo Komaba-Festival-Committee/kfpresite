e2f5c874e6b76d088726cb30eed02f2d7e1ec144
gderecho <gderecho@users.noreply.github.com>
2026-06-25 10:58:10 +0900

    [fix] Reauthorize upon incorrect credentials by setting the www authenticate header

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -11,6 +11,12 @@ import { env } from "cloudflare:workers"
 
 const BASIC_USER = env.PRE_QA_USER
 const BASIC_PASS = env.PRE_QA_PASS
+const RESPONSE_UNAUTHORIZED = {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="QA Test", charset="UTF-8"',
+    }
+}
 
 async function errorHandling(context) {
   try {
@@ -25,9 +31,7 @@ async function handleRequest({ next, request }) {
     const Authorization = request.headers.get('Authorization')
     const [scheme, encoded] = Authorization.split(' ')
     if (!encoded || scheme !== 'Basic') {
-      return new Response(`Invalid authorization header`, {
-        status: 400,
-      })
+      return new Response(`Invalid authorization header`, RESPONSE_UNAUTHORIZED)
     }
 
     const buffer = Uint8Array.from(atob(encoded), (character) =>
@@ -38,29 +42,24 @@ async function handleRequest({ next, request }) {
     const index = decoded.indexOf(':')
 
     if (index === -1 || /[\0-\x1F\x7F]/.test(decoded)) {
-      return new Response('Invalid authorization value.', { status: 400 })
+      return new Response('Invalid authorization value.', RESPONSE_UNAUTHORIZED)
     }
 
     const user = decoded.substring(0, index);
     const pass = decoded.substring(index + 1);
 
     if (BASIC_USER !== user) {
-      return new Response('Invalid credentials.', { status: 401 })
+      return new Response('Invalid credentials.', RESPONSE_UNAUTHORIZED)
     }
 
     if (BASIC_PASS !== pass) {
-      return new Response('Invalid credentials.', { status: 401 })
+      return new Response('Invalid credentials.', RESPONSE_UNAUTHORIZED)
     }
 
     return await next()
   }
 
-  return new Response('Authentication required.', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="my scope", charset="UTF-8"',
-    },
-  })
+  return new Response('Authentication required.', RESPONSE_UNAUTHORIZED)
 }
 
 export const onRequest = [errorHandling, handleRequest]
