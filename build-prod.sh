af3f47f15f7f5468202ff98b4d0c0c63fb09b61e
gderecho <gderecho@users.noreply.github.com>
2026-07-02 16:33:37 +0900

    [fix] Fix build script

--- build-prod.sh
+++ build-prod.sh
@@ -6,14 +6,14 @@ echo "/ /77/ 301" > .output/production/_redirects
 
 bash -cx "cd 77;
 rm -rf .nuxt .output node-modules;
-pnpm i;
+pnpm install --frozen-lockfile;
 pnpm generate:production;"
 
 cp -r 77/.output/public .output/production/77
 
 bash -cx "cd 76;
 rm -rf .nuxt .output node-modules;
-yarn install;
+yarn install --frozen-lockfile;
 yarn generate:production;"
 
 cp -r 76/.output/public .output/production/76
